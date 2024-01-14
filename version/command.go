204f24c625d90cb32c88f31f0dc4dd2f8e5c28ba
Scott Long <smxlong@gmail.com>
2024-01-14 14:32:43 -0800

    split version command into its own file

---
+++ version/command.go
@@ -0,0 +1,20 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// VersionCommand returns the version command.
+func VersionCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Display version",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println(Version)
+			return nil
+		},
+	}
+	return cmd
+}
--- version/version.go
+++ version/version.go
@@ -1,22 +1,3 @@
 package version
 
-import (
-	"fmt"
-
-	"github.com/spf13/cobra"
-)
-
 const Version = "0.1.0"
-
-// versionCommand returns the version command.
-func VersionCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "version",
-		Short: "Display version",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println(Version)
-			return nil
-		},
-	}
-	return cmd
-}
